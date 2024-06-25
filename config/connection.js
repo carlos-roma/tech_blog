1284e54c3afb043514025a4541edd1406fbd64bf
carlos-roma <adrianc.rm0@gmail.com>
2024-06-25 05:49:53 -0600

    modifying config to deploy

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,24 @@
-const Sequelize = require('sequelize');
+// connection.js
 require('dotenv').config();
+const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST || 'localhost',
-    dialect: process.env.DB_DIALECT || 'mysql',
-    port: process.env.DB_PORT || 3306,
-  }
-);
+let sequelize;
+
+if (process.env.CLEARDB_DATABASE_URL) {
+  // For deployment on Heroku
+  sequelize = new Sequelize(process.env.CLEARDB_DATABASE_URL);
+} else {
+  // For local development
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: process.env.DB_HOST,
+      dialect: 'mysql',
+      port: 3306,
+    }
+  );
+}
 
 module.exports = sequelize;
